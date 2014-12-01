9897fb534fbb529ac9c33a6f9206df82ade8a5f9
theory friction practice <jackson.brebnor@gmail.com>
2014-12-01 17:35:38 -0500

    places static assets lines into each environment becase bullshit

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+    config.serve_static_assets = true
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,7 +10,7 @@ Bundler.require(*Rails.groups)
 module JSRosen01
   class Application < Rails::Application
 
-    config.serve_static_assets = true
+   # config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
