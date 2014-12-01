e686777580c83aade7467eda65ddf00e3084e71b
theory friction practice <jackson.brebnor@gmail.com>
2014-12-01 16:25:18 -0500

    follows heroku's instructions for precompiling assets

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-config.serve_static_assets = true
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module JSRosen01
   class Application < Rails::Application
+
+    config.serve_static_assets = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
